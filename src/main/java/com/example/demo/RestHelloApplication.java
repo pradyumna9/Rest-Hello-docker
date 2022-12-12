d2fd56b61cb7f2895919229f041cc2cbce434317
Pradyumna <tech.daspk@gmail.com>
2022-12-12 22:34:58 +0000

    Initial commit

---
+++ src/main/java/com/example/demo/RestHelloApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestHelloApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestHelloApplication.class, args);
+	}
+
+}
